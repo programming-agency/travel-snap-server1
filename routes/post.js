8e51896a10e02a6c1cb1a16d0afa4dae16c76d91
HASHEBUR RAHMAN <mdhasheburrahman3@gmail.com>
2023-10-04 10:42:36 +0600

    my post filter

--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,6 @@ const postRoutes = (app) => {
         try {
             const data = req.body
             const { title, userName, content, country } = data
-
             if (!title || !userName || !content || !country) return res.status(401).json({ message: 'title, userName, content are required!' })
             const createdPost = new Post(data).save()
             res.status(200).json({ message: 'Post created successfully!', post: createdPost })
@@ -26,12 +25,9 @@ const postRoutes = (app) => {
         }
     })
 
-    // destination 
-
-    // Create a route to fetch and filter posts by country
+    // destination     
     app.get('/api/posts/:country', async (req, res) => {
         const country = req.params.country;
-
         try {
             // Use the 'find' method to filter posts by the 'country' field
             const posts = await Post.find({ country: country });
@@ -40,38 +36,43 @@ const postRoutes = (app) => {
                 // Handle the case where no matching posts are found
                 return res.status(404).json({ message: 'No posts found for the specified country.' });
             }
-
             res.status(200).json(posts);
         } catch (error) {
             console.error('Error fetching posts by country:', error);
             res.status(500).json({ message: 'Something went wrong!' });
         }
     });
-    // app.get('/api/posts/:country', async (req, res) => {
-    //     const country = req.params.country;
+
+    //  get my post
+    // app.get('/api/posts/:userName', async (req, res) => {
+    //     const userName = req.params.userName;
     //     try {
-    //         const posts = await Post.find({ country: country });
+    //         // Use the 'find' method to filter posts by the 'username' field of the author
+    //         const myPosts = await Post.find({ authorUsername: userName });
+
+    //         if (myPosts.length === 0) {
+    //             // Handle the case where no matching posts are found
+    //             return res.status(404).json({ message: 'No posts found for the specified username.' });
+    //         }
     //         res.status(200).json(posts);
     //     } catch (error) {
-    //         console.error('Error fetching posts by country:', error);
+    //         console.error('Error fetching posts by username:', error);
     //         res.status(500).json({ message: 'Something went wrong!' });
     //     }
     // });
 
-    //  get my post
-    app.get('/api/posts/username/:userName', async (req, res) => {
-        const username = req.params.username;
-
+    app.get('/api/posts/:userName', async (req, res) => {
+        const userName = req.params.userName;
         try {
-            // Use the 'find' method to filter posts by the 'username' field of the author
-            const posts = await Post.find({ authorUsername: username });
+            // Use the 'find' method to filter posts by the 'authorUsername' field
+            const myPosts = await Post.find({ authorUsername: userName });
+            
 
-            if (posts.length === 0) {
+            if (myPosts.length === 0) {
                 // Handle the case where no matching posts are found
                 return res.status(404).json({ message: 'No posts found for the specified username.' });
             }
-
-            res.status(200).json(posts);
+            res.status(200).json(myPosts); // Return myPosts instead of posts
         } catch (error) {
             console.error('Error fetching posts by username:', error);
             res.status(500).json({ message: 'Something went wrong!' });
@@ -82,16 +83,14 @@ const postRoutes = (app) => {
 
     app.delete('/api/posts/:id', async (req, res) => {
         try {
-            const deletePost = await Post.findByIdAndDelete(req.params._id)
+            const deletePost = await Post.findByIdAndDelete(req.params.id)
+            // console.log(deletePost);
             res.status(200).json({ message: 'Student deleted successfully' })
         } catch (error) {
             res.status(500).json(error?.message)
-            // console.log(error);
+            console.log(error);
         }
     })
 }
-
-
-
 
 module.exports = postRoutes;
